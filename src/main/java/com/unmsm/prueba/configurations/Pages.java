d216e1dc02e0006ed6ed0f4565d5620b8da16362
Jorge <jorge.meza9@unmsm.edu.pe>
2020-03-10 23:41:14 -0500

    Metodo Post con un formulario

--- src/main/java/com/unmsm/prueba/configurations/Pages.java
+++ src/main/java/com/unmsm/prueba/configurations/Pages.java
@@ -3,4 +3,5 @@ package com.unmsm.prueba.configurations;
 public class Pages {
 	public static String HOME = "index";
 	public static String POST = "post";
+	public static String FORM1 = "form1";
 }
--- src/main/java/com/unmsm/prueba/controllers/ControllerBasic.java
+++ src/main/java/com/unmsm/prueba/controllers/ControllerBasic.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -45,5 +46,20 @@ public class ControllerBasic {
 		
 		post.addObject("posts", postFiltrado.get(0));
 		return post;
+	}
+	
+	@GetMapping(path = {"/form1"})
+	public ModelAndView getForm() {
+		ModelAndView post = new ModelAndView(Pages.FORM1);
+		post.addObject("post", new Post());
+		return post;
+	}
+	
+	@PostMapping("/form1/newPost")
+	public String addNewPost(Post post, Model model) {
+		List<Post> posts = this.getPost();
+		posts.add(post);
+		model.addAttribute("posts", posts);
+		return Pages.HOME;
 	}
 }
